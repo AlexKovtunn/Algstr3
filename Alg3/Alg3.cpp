1964d5ffaa57111f4bc8989986924708d39993ea
AlexKovtunn <can9i2000@bk.ru>
2020-12-25 02:33:02 +0500

    1

---
+++ Alg3/Alg3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "bst.h"
+
+using namespace std;
+
+int main() {
+	BST bst;
+	int arr[10] = { 8, 3, 10, 1, 6, 4, 7, 14, 13, 15 };
+	for (int i = 0; i < 10; i++) bst.insert(arr[i]);
+	Iterator* iter = bst.create_dft_iterator();
+	for (int i = 0; i < 10; i++) {
+		cout << iter->next() << " ";
+		cout << bst.contains(arr[i]) << endl;
+	}
+	return 0;
+}
